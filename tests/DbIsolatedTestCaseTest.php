6c385a4b86c3a767242e78d4f7e56852bbd05f5f
andreyserdjuk <ase6120@gmail.com>
2017-04-06 23:32:52 +0300

    db isolated test case added shared conn

--- tests/DbIsolatedTestCaseTest.php
+++ tests/DbIsolatedTestCaseTest.php
@@ -7,6 +7,7 @@ use AndreySerdjuk\DecoupledFuncTesting\DbIsolatedTestCase;
 use AndreySerdjuk\DecoupledFuncTesting\DbIsolation;
 use AndreySerdjuk\DecoupledFuncTesting\DbIsolationAnnotation;
 use AndreySerdjuk\DecoupledFuncTesting\DbIsolationHandler;
+use Doctrine\Tests\DbUtil;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -15,6 +16,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class DbIsolatedTestCaseTest extends DbIsolatedTestCase
 {
     use CustomKernelTrait;
+
+    /**
+     * Shared connection when a TestCase is run alone (outside of its functional suite).
+     *
+     * @var \Doctrine\DBAL\Connection|null
+     */
+    private static $sharedConn;
+
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $conn;
+
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        if (!isset(self::$sharedConn)) {
+            self::$sharedConn = DbUtil::getConnection();
+        }
+        $this->conn = self::$sharedConn;
+    }
 
     public function testRealTransaction()
     {
@@ -28,5 +52,13 @@ class DbIsolatedTestCaseTest extends DbIsolatedTestCase
             'test_case' => 'DbIsolation',
             'root_config' => 'config.yml',
         ];
+    }
+
+    protected function resetSharedConn()
+    {
+        if (self::$sharedConn) {
+            self::$sharedConn->close();
+            self::$sharedConn = null;
+        }
     }
 }
